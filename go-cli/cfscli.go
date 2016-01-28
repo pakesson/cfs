72b31d0e53f49a471d5e5311ad36950ccc67a1d5
Philip Åkesson <philip.akesson@gmail.com>
2016-01-28 20:20:30 +0100

    Run gofmt on the Go server and Go CLI client

--- go-cli/cfscli.go
+++ go-cli/cfscli.go
@@ -1,227 +1,227 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "io"
-    "bytes"
-    //"time"
-    //"strconv"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	//"time"
+	//"strconv"
 
-    "net/http"
-    "net/url"
-    "io/ioutil"
-    "path/filepath"
-    "encoding/json"
-    "encoding/base64"
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path/filepath"
 
-    "crypto/sha256"
-    "crypto/rand"
+	"crypto/rand"
+	"crypto/sha256"
 
-    // External deps
-    // Check the code in $GOPATH after running 'go get'
-    "golang.org/x/crypto/nacl/secretbox" // This is official (as far as I can tell), but in a separate repository
-    "github.com/bgentry/speakeasy" // This is not much code. Can be easily integrated.
+	// External deps
+	// Check the code in $GOPATH after running 'go get'
+	"github.com/bgentry/speakeasy"       // This is not much code. Can be easily integrated.
+	"golang.org/x/crypto/nacl/secretbox" // This is official (as far as I can tell), but in a separate repository
 )
 
 const (
-    KEY_SIZE   = 32
-    NONCE_SIZE = 24
+	KEY_SIZE   = 32
+	NONCE_SIZE = 24
 
-    BASE_URL = "http://localhost:5000"
+	BASE_URL = "http://localhost:5000"
 )
 
 type Upload_info struct {
-    Url string
-    Key string
+	Url string
+	Key string
 }
 
 type Download_info struct {
-    Url string
+	Url string
 }
 
 func usage(name string) {
-    fmt.Println("Cloud Crypto File Cloud Storage for the Cloud(tm) Go Command Line Client (CCFCSCGCLC)")
-    fmt.Println("")
-    fmt.Printf("Usage: %v (upload|download) [...]\n", name)
-    fmt.Printf("   %v upload filename - Upload the file 'filename'\n", name)
-    fmt.Printf("   %v download key - Upload the file with key 'key'\n", name)
+	fmt.Println("Cloud Crypto File Cloud Storage for the Cloud(tm) Go Command Line Client (CCFCSCGCLC)")
+	fmt.Println("")
+	fmt.Printf("Usage: %v (upload|download) [...]\n", name)
+	fmt.Printf("   %v upload filename - Upload the file 'filename'\n", name)
+	fmt.Printf("   %v download key - Upload the file with key 'key'\n", name)
 }
 
 func oopsie(msg string) {
-    fmt.Fprintf(os.Stderr, "%v\n", msg)
-    os.Exit(1)
+	fmt.Fprintf(os.Stderr, "%v\n", msg)
+	os.Exit(1)
 }
 
 func create_nonce() (*[NONCE_SIZE]byte, error) {
-    nonce := new([NONCE_SIZE]byte)
-    _, err := io.ReadFull(rand.Reader, nonce[:])
-    if err != nil {
-        return nil, err
-    }
-    return nonce, nil
+	nonce := new([NONCE_SIZE]byte)
+	_, err := io.ReadFull(rand.Reader, nonce[:])
+	if err != nil {
+		return nil, err
+	}
+	return nonce, nil
 }
 
 func upload(file string, password string) {
-    filename := filepath.Base(file)
+	filename := filepath.Base(file)
 
-    cipher_key := sha256.Sum256([]byte(password))
+	cipher_key := sha256.Sum256([]byte(password))
 
-    nonce, err := create_nonce()
-    if err != nil {
-        oopsie(err.Error())
-    }
+	nonce, err := create_nonce()
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    encrypted_filename := make([]byte, len(nonce))
-    copy(encrypted_filename, nonce[:])
-    encrypted_filename = secretbox.Seal(encrypted_filename, []byte(filename), nonce, &cipher_key)
-    encoded_filename := base64.StdEncoding.EncodeToString(encrypted_filename)
+	encrypted_filename := make([]byte, len(nonce))
+	copy(encrypted_filename, nonce[:])
+	encrypted_filename = secretbox.Seal(encrypted_filename, []byte(filename), nonce, &cipher_key)
+	encoded_filename := base64.StdEncoding.EncodeToString(encrypted_filename)
 
-    data, err := ioutil.ReadFile(file)
-    if err != nil {
-        oopsie(err.Error())
-    }
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    nonce, err = create_nonce()
-    if err != nil {
-        oopsie(err.Error())
-    }
+	nonce, err = create_nonce()
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    encrypted_data := make([]byte, len(nonce))
-    copy(encrypted_data, nonce[:])
-    encrypted_data = secretbox.Seal(encrypted_data, data, nonce, &cipher_key)
+	encrypted_data := make([]byte, len(nonce))
+	copy(encrypted_data, nonce[:])
+	encrypted_data = secretbox.Seal(encrypted_data, data, nonce, &cipher_key)
 
-    uri, err := url.ParseRequestURI(BASE_URL)
-    uri.Path = "/api/upload"
-    query_parameters := url.Values{}
-    query_parameters.Set("filename", encoded_filename)
-    uri.RawQuery = query_parameters.Encode()
-    url_str := fmt.Sprintf("%v", uri)
+	uri, err := url.ParseRequestURI(BASE_URL)
+	uri.Path = "/api/upload"
+	query_parameters := url.Values{}
+	query_parameters.Set("filename", encoded_filename)
+	uri.RawQuery = query_parameters.Encode()
+	url_str := fmt.Sprintf("%v", uri)
 
-    res, err := http.Get(url_str)
-    if err != nil {
-        oopsie(err.Error())
-    }
-    defer res.Body.Close()
+	res, err := http.Get(url_str)
+	if err != nil {
+		oopsie(err.Error())
+	}
+	defer res.Body.Close()
 
-    decoder := json.NewDecoder(res.Body)
-    var jsondata Upload_info
-    err = decoder.Decode(&jsondata)
-    if err != nil {
-        oopsie(err.Error())
-    }
+	decoder := json.NewDecoder(res.Body)
+	var jsondata Upload_info
+	err = decoder.Decode(&jsondata)
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    url := jsondata.Url
+	url := jsondata.Url
 
-    r := bytes.NewReader(encrypted_data)
-    request, err := http.NewRequest("PUT", url, r)
-    if err != nil {
-        oopsie(err.Error())
-    }
+	r := bytes.NewReader(encrypted_data)
+	request, err := http.NewRequest("PUT", url, r)
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    request.Header.Set("x-amz-acl", "private")
-    request.Header.Set("x-amz-meta-filename", encoded_filename)
-    //request.Header.Set("expires", strconv.FormatInt(time.Now().Unix() + 60*60, 10)) // TODO: This should probably be datetime(now + 1 hour)
+	request.Header.Set("x-amz-acl", "private")
+	request.Header.Set("x-amz-meta-filename", encoded_filename)
+	//request.Header.Set("expires", strconv.FormatInt(time.Now().Unix() + 60*60, 10)) // TODO: This should probably be datetime(now + 1 hour)
 
-    client := &http.Client{}
-    res, err = client.Do(request)
-    if err != nil {
-        oopsie(err.Error())
-    }
+	client := &http.Client{}
+	res, err = client.Do(request)
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    //defer res.Body.Close()
-    //body, err := ioutil.ReadAll(res.Body)
-    //fmt.Printf("%s\n", string(body))
+	//defer res.Body.Close()
+	//body, err := ioutil.ReadAll(res.Body)
+	//fmt.Printf("%s\n", string(body))
 
-    if res.StatusCode == http.StatusOK {
-        fmt.Println("File uploaded.")
-        fmt.Printf("Download URL: http://127.0.0.1:5000/download/%v\n", jsondata.Key)
-    }
+	if res.StatusCode == http.StatusOK {
+		fmt.Println("File uploaded.")
+		fmt.Printf("Download URL: http://127.0.0.1:5000/download/%v\n", jsondata.Key)
+	}
 }
 
 func download(key string, password string) {
-    fmt.Println("Downloading file")
+	fmt.Println("Downloading file")
 
-    cipher_key := sha256.Sum256([]byte(password))
+	cipher_key := sha256.Sum256([]byte(password))
 
-    uri, err := url.ParseRequestURI(BASE_URL)
-    uri.Path = "/api/download"
-    query_parameters := url.Values{}
-    query_parameters.Set("key", key)
-    uri.RawQuery = query_parameters.Encode()
-    url_str := fmt.Sprintf("%v", uri)
+	uri, err := url.ParseRequestURI(BASE_URL)
+	uri.Path = "/api/download"
+	query_parameters := url.Values{}
+	query_parameters.Set("key", key)
+	uri.RawQuery = query_parameters.Encode()
+	url_str := fmt.Sprintf("%v", uri)
 
-    res, err := http.Get(url_str)
-    if err != nil {
-        oopsie(err.Error())
-    }
-    defer res.Body.Close()
+	res, err := http.Get(url_str)
+	if err != nil {
+		oopsie(err.Error())
+	}
+	defer res.Body.Close()
 
-    decoder := json.NewDecoder(res.Body)
-    var jsondata Download_info
-    err = decoder.Decode(&jsondata)
-    if err != nil {
-        oopsie(err.Error())
-    }
+	decoder := json.NewDecoder(res.Body)
+	var jsondata Download_info
+	err = decoder.Decode(&jsondata)
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    url := jsondata.Url
-    res, err = http.Get(url)
-    if err != nil {
-        oopsie(err.Error())
-    }
-    defer res.Body.Close()
+	url := jsondata.Url
+	res, err = http.Get(url)
+	if err != nil {
+		oopsie(err.Error())
+	}
+	defer res.Body.Close()
 
-    encoded_metadata := res.Header.Get("x-amz-meta-filename")
-    metadata, err := base64.StdEncoding.DecodeString(encoded_metadata)
-    if err != nil {
-        oopsie(err.Error())
-    }
+	encoded_metadata := res.Header.Get("x-amz-meta-filename")
+	metadata, err := base64.StdEncoding.DecodeString(encoded_metadata)
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    var nonce [NONCE_SIZE]byte
-    copy(nonce[:], metadata)
-    filename_bytes, ok := secretbox.Open(nil, metadata[NONCE_SIZE:], &nonce, &cipher_key)
-    if !ok { // Thanks for returning a bool instead of an error object :( Super intuitive!
-        oopsie(err.Error())
-    }
-    filename := string(filename_bytes)
+	var nonce [NONCE_SIZE]byte
+	copy(nonce[:], metadata)
+	filename_bytes, ok := secretbox.Open(nil, metadata[NONCE_SIZE:], &nonce, &cipher_key)
+	if !ok { // Thanks for returning a bool instead of an error object :( Super intuitive!
+		oopsie(err.Error())
+	}
+	filename := string(filename_bytes)
 
-    file, err := os.Create(filename)
-    if err != nil {
-        oopsie(err.Error())
-    }
-    defer file.Close()
+	file, err := os.Create(filename)
+	if err != nil {
+		oopsie(err.Error())
+	}
+	defer file.Close()
 
-    _, err = io.Copy(file, res.Body)
-    if err != nil {
-        oopsie(err.Error())
-    }
+	_, err = io.Copy(file, res.Body)
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    fmt.Printf("File saved as '%v'\n", filename)
+	fmt.Printf("File saved as '%v'\n", filename)
 }
 
 func main() {
-    prog := os.Args[0]
+	prog := os.Args[0]
 
-    if len(os.Args) != 3 {
-        usage(prog)
-        os.Exit(1)
-    }
+	if len(os.Args) != 3 {
+		usage(prog)
+		os.Exit(1)
+	}
 
-    verb := os.Args[1]
-    object := os.Args[2]
+	verb := os.Args[1]
+	object := os.Args[2]
 
-    password, err := speakeasy.Ask("Password: ")
-    if err != nil {
-        oopsie(err.Error())
-    }
+	password, err := speakeasy.Ask("Password: ")
+	if err != nil {
+		oopsie(err.Error())
+	}
 
-    switch verb {
-        case "upload":
-            upload(object, password)
-        case "download":
-            download(object, password)
-        default:
-            usage(prog)
-            os.Exit(1)
-    }
+	switch verb {
+	case "upload":
+		upload(object, password)
+	case "download":
+		download(object, password)
+	default:
+		usage(prog)
+		os.Exit(1)
+	}
 }
--- go-server/server.go
+++ go-server/server.go
@@ -1,113 +1,113 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "time"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-    "net/http"
-    "encoding/json"
+	"encoding/json"
+	"net/http"
 
-    "crypto/rand"
+	"crypto/rand"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 const (
-    UUID_SIZE = 16
+	UUID_SIZE = 16
 )
 
 type DownloadResponse struct {
-    Url     string
+	Url string
 }
 
 type UploadResponse struct {
-    Key     string
-    Url     string
+	Key string
+	Url string
 }
 
 func oopsie(msg string) {
-    log.Fatal(msg)
-    os.Exit(1)
+	log.Fatal(msg)
+	os.Exit(1)
 }
 
 func uuid4() string {
-    buf := make([]byte, 16)
-    if _, err := rand.Read(buf); err != nil {
-        panic(err)
-    }
-    return fmt.Sprintf("%08x-%04x-%04x-%04x-%12x", buf[0:4], buf[4:6], buf[6:8],
-                                                   buf[8:10], buf[10:16])
+	buf := make([]byte, 16)
+	if _, err := rand.Read(buf); err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%08x-%04x-%04x-%04x-%12x", buf[0:4], buf[4:6], buf[6:8],
+		buf[8:10], buf[10:16])
 }
 
 func api_download(w http.ResponseWriter, r *http.Request) {
-    bucket := os.Getenv("S3_BUCKET")
-    region := os.Getenv("AWS_REGION")
+	bucket := os.Getenv("S3_BUCKET")
+	region := os.Getenv("AWS_REGION")
 
-    key := r.URL.Query().Get("key")
-    log.Printf("Download request for '%v'", key)
+	key := r.URL.Query().Get("key")
+	log.Printf("Download request for '%v'", key)
 
-    svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
-    resp, err := svc.HeadObject(&s3.HeadObjectInput{
-        Bucket: aws.String(bucket),
-        Key:    aws.String(key),
-    })
+	svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
+	resp, err := svc.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
 
-    log.Println(resp)
+	log.Println(resp)
 
-    if err != nil {
-        log.Println("Failed to get object", err)
-    }
+	if err != nil {
+		log.Println("Failed to get object", err)
+	}
 
-    req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
-        Bucket: aws.String(bucket),
-        Key:    aws.String(key),
-    })
-    url, err := req.Presign(15 * time.Minute)
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	url, err := req.Presign(15 * time.Minute)
 
-    if err != nil {
-        log.Println("Failed to sign request", err)
-    }
+	if err != nil {
+		log.Println("Failed to sign request", err)
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    response := DownloadResponse{Url: url}
-    json.NewEncoder(w).Encode(response)
+	w.Header().Set("Content-Type", "application/json")
+	response := DownloadResponse{Url: url}
+	json.NewEncoder(w).Encode(response)
 }
 
 func api_upload(w http.ResponseWriter, r *http.Request) {
-    filename := r.URL.Query().Get("filename")
-    log.Printf("Upload request for '%v'", filename)
+	filename := r.URL.Query().Get("filename")
+	log.Printf("Upload request for '%v'", filename)
 
-    key := uuid4()
-    bucket := os.Getenv("S3_BUCKET")
-    region := os.Getenv("AWS_REGION")
+	key := uuid4()
+	bucket := os.Getenv("S3_BUCKET")
+	region := os.Getenv("AWS_REGION")
 
-    svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
-    req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
-        Bucket: aws.String(bucket),
-        Key:    aws.String(key),
-        ACL:    aws.String("private"),
-        Metadata: map[string]*string{
-            "filename": aws.String(filename),
-        },
-    })
-    url, err := req.Presign(15 * time.Minute)
+	svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
+	req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		ACL:    aws.String("private"),
+		Metadata: map[string]*string{
+			"filename": aws.String(filename),
+		},
+	})
+	url, err := req.Presign(15 * time.Minute)
 
-    if err != nil {
-        log.Println("Failed to sign request", err)
-    }
+	if err != nil {
+		log.Println("Failed to sign request", err)
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    response := UploadResponse{Key: key, Url: url}
-    json.NewEncoder(w).Encode(response)
+	w.Header().Set("Content-Type", "application/json")
+	response := UploadResponse{Key: key, Url: url}
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-    fmt.Println("Starting server")
-    http.HandleFunc("/api/download", api_download)
-    http.HandleFunc("/api/upload", api_upload)
-    log.Fatal(http.ListenAndServe(":5000", nil))
+	fmt.Println("Starting server")
+	http.HandleFunc("/api/download", api_download)
+	http.HandleFunc("/api/upload", api_upload)
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
